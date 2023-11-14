a867d32d72f5219cadf6f41c7ae154d3633c1762
Dmitriy Vagin <dvagin@avito.ru>
2023-11-14 12:58:33 +0300

    copy example

---
+++ copy/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	var err error
+	defer func() {
+		if err != nil {
+			slog.Error("error", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	db1, err := pgx.Connect(ctx, "postgresql://localhost/db1")
+	if err != nil {
+		return
+	}
+	defer db1.Close(ctx)
+
+	db2, err := pgx.Connect(ctx, "postgresql://localhost/db2")
+	if err != nil {
+		return
+	}
+	defer db2.Close(ctx)
+
+	r, w := io.Pipe()
+
+	doneChan := make(chan struct{}, 1)
+
+	go func() {
+		defer close(doneChan)
+
+		_, err := db1.PgConn().CopyTo(ctx, w, `copy table1 to stdin binary`)
+		if err != nil {
+			slog.Error("error", "error", err)
+			return
+		}
+		_ = w.Close()
+		doneChan <- struct{}{}
+	}()
+
+	_, err = db2.PgConn().CopyFrom(ctx, r, `copy table1 from stdout binary`)
+	_ = r.Close()
+
+	select {
+	case <-doneChan:
+	case <-ctx.Done():
+	}
+
+	return
+}
